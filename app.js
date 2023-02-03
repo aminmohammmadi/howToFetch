bc12d30b89b70ad4e81bd565a1873c11956208d0
Amin <macbookpro13@Amin-Mohammadi.local>
2023-02-03 10:50:04 +0330

    the GET and POST and PUT and DELETE buttons are made and tested

--- app.js
+++ app.js
@@ -7,44 +7,53 @@ theButton.onclick = () => {
     .then(data => console.log(data))
 }
 
-const sendButton = document.getElementById('sendData')
-sendData.onclick =()=>{
-    console.log('data successfully sent')
-    fetch ('http://localhost:3000/employees', {
-        method: "POST", 
-        headers: {
-            'Content-Type': 'application/json'},
+// const sendButton = document.getElementById('sendData')
+// sendData.onclick =()=>{
+//     console.log('data successfully sent')
+//     fetch ('http://localhost:3000/employees', {
+//         method: "POST", 
+//         headers: {
+//             'Content-Type': 'application/json'},
         
-            body: JSON.stringify({"name": "Hossein"})
-        //     body: JSON.stringify ({
-        //     "name": "Hossein"
-        // })
+//             body: JSON.stringify ({
+//             "name": "Hossein",
+//             "id": 5
+//         })
+//     })
+//     .then(response=>response.json())
+//     .then(data=>console.log(data))
+// }
+
+const postButton = document.getElementById('sendData')
+postButton.onclick =()=>{
+    console.log('data successfully sent')
+    fetch('http://localhost:3000/employees',{
+        method: "POST",
+        headers:{'content-type': 'application/json'},
+        body: JSON.stringify({
+            "name": "Zagros",
+             "id": 2
+            })
     })
-    .then(response=>response.json())
+    .then(response => response.json())
     .then(data=>console.log(data))
 }
 
-// const editButton = document.getElementById('editData')
-// editButton.onclick = ()=>{
-//     console.log('successfully edited')
-//     fetch('http://localhost:3000/people/4', {
-//         method: "PUT",
-//         headers: {
-//             "Content-Type": "application/json" 
-//         },
-//         body: JSON.stringify ({
-//             "name": "AbdolRahman"
-//         })
-//     })
-//     .then (response=>response.json())
-//     .then (data => console.log(data))
-// }
-// const deleteButton = document.getElementById('deleteData')
-// deleteButton.onclick = ()=> {
-//     console.log('successfully deleted')
-//     fetch('http://localhost:3000/people/2', {
-//         method: "DELETE",
-//     })
-//     .then(response => response.json())
-//     .then(data => console.log(data))
-// }
+const putButton = document.getElementById('editData')
+putButton.onclick =()=>{
+    console.log('data successfuly edited')
+    fetch('http://localhost:3000/employees/2',{
+        method: "PUT",
+        headers: { 'content-type': 'application/json'},
+        body:  JSON.stringify({ "name": "Sina"})
+    })
+}
+
+const deleteButton = document.getElementById('deleteData')
+deleteButton.onclick =()=>{
+    console.log('data is deleted')
+    fetch('http://localhost:3000/employees/2',{
+        method: 'DELETE',
+    })
+}
+
